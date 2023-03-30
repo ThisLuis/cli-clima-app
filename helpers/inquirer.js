c331b2caf9798f531c78aa3c4471517790ef8947
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-03-29 22:49:04 -0600

    Creacion del menu interactivo de la consola, aun no es funcional cada opcion, solo la de salir

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -9,34 +9,17 @@ const questions = [
         message: '¿Que desea hacer?',
         choices: [
             {
-                value: '1',
-                name: `${ '1.'.green } Crear tarea`,
+                value: 1,
+                name: `${ '1.'.green } Buscar Ciudad`,
             },
             {
-                value: '2',
-                name: `${ '2.'.green } Listar tareas`,
+                value: 2,
+                name: `${ '2.'.green } Historial De Busquedas`,
             },
             {
-                value: '3',
-                name: `${ '3.'.green } Listar tareas completadas`,
-            },
-            {
-                value: '4',
-                name: `${ '4.'.green } Listar tareas pendientes`,
-            },
-            {
-                value: '5',
-                name: `${ '5.'.green } Completar tarea`,
-            },
-            {
-                value: '6',
-                name: `${ '6.'.green } Borrar tarea`,
-            },
-            {
-                value: '0',
+                value: 0,
                 name: `${ '0.'.green } Salir`,
-            },
-            
+            }
         ]
     }
 ]
@@ -58,7 +41,7 @@ const pause = async() => {
         {
             type: 'input',
             name: 'enter',
-            message: `Presione ${ 'ENTER'.green } para continur...`,
+            message: `Presione ${ 'ENTER'.green } para continuar...`,
         }
     ];
     console.log('\n');
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import { readInput } from './helpers/inquirer.js';
+import { inquirerMenu, pause, readInput } from './helpers/inquirer.js';
 
 const main = async() => {
-    const text = await readInput('Hola: ');
-    console.log( text );
+   
+    let opt;
+    
+    do {
+        opt = await inquirerMenu();
+
+        switch ( opt ) {
+            case 1:
+                console.log('Seleccionaste la opcion 1');
+            break;
+
+            case 2:
+                console.log('Seleccionaste la opcion 2');
+            break;
+
+            case 0:
+                console.log('Seleccionaste la opcion 3');
+            break; 
+        }
+
+        if ( opt !== 0) await pause();
+
+    } while( opt !== 0);
 }
 
 main();
